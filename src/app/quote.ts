ddfd6291634de5de1728485698eb6ffbd06ad298
ynyanchoka <monaryymelda@gmail.com>
2022-04-08 15:37:27 +0300

    add license

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,16 @@
 export class Quote {
+    showDetails: boolean;
+    showMore: boolean;
+    constructor(
+        public quote: string,
+        public author: string,
+        public submittedBy: string,
+        public submissionDate: Date,
+        public upVote: number,
+        public downVote: number,
+        public smallText: any = 'see more') {
+      this.showDetails = false;
+      this.showMore = false;
+    }
+
 }
